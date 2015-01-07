1efc7bad0a1a84ccc5061a7a0e16e12c3d23d1d6
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2015-01-06 23:47:27 -0200

    Ajsute da namespace do Facades

--- src/laravel/pagseguro/Facades/PagSeguro.php
+++ src/laravel/pagseguro/Facades/PagSeguro.php
@@ -9,5 +9,5 @@ class PagSeguro extends Facade{
      *
      * @return string
      */
-    protected static function getFacadeAccessor() { return 'pagseguro'; }
+    protected static function getFacadeAccessor() { return 'PaymentRequest'; }
 } 
