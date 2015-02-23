49be0d78c0cd14443c04b9d3f53efc3d291c8b07
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2015-02-23 00:57:59 -0300

    Criação do separador e da requisição de compra ao PagSeguro

--- src/laravel/pagseguro/Complements/DataRequestHydrator.php
+++ src/laravel/pagseguro/Complements/DataRequestHydrator.php
@@ -12,15 +12,15 @@ trait DataRequestHydrator
     
     private function getItemsSeparator($data)
     {
-        $i = 0;
+        $numberItem = 0;
         foreach ($data->data['items'] as $key => $value) {
-            $i++;
-            $dataItem["itemId$i"] = (array_key_exists($key, $value) ? $value[$key] : null);
-            $dataItem["itemDescription$i"] = (array_key_exists("itemDescription$i", $value) ? $value["itemDescription$i"] : null);
-            $dataItem["itemQuantity$i"] = (array_key_exists("itemQuantity$i", $value) ? $value["itemQuantity$i"] : null);
-            $dataItem["itemWeight$i"] = (array_key_exists("itemWeight$i", $value) ? $value["itemWeight$i"] : null);
-            $dataItem["itemShippingCost$i"] = (array_key_exists("itemShippingCost$i", $value) ? $value["itemShippingCost$i"] : null);
-            $dataItem["itemAmount$i"] = (array_key_exists("itemAmount$i", $value) ? $value["itemAmount$i"] : null);
+            $numberItem++;
+            $dataItem["itemId$numberItem"] = (array_key_exists($key, $value) ? $value[$key] : null);
+            $dataItem["itemDescription$numberItem"] = (array_key_exists("itemDescription$numberItem", $value) ? $value["itemDescription$numberItem"] : null);
+            $dataItem["itemQuantity$numberItem"] = (array_key_exists("itemQuantity$numberItem", $value) ? $value["itemQuantity$numberItem"] : null);
+            $dataItem["itemWeight$numberItem"] = (array_key_exists("itemWeight$numberItem", $value) ? $value["itemWeight$numberItem"] : null);
+            $dataItem["itemShippingCost$numberItem"] = (array_key_exists("itemShippingCost$numberItem", $value) ? $value["itemShippingCost$numberItem"] : null);
+            $dataItem["itemAmount$numberItem"] = (array_key_exists("itemAmount$numberItem", $value) ? $value["itemAmount$numberItem"] : null);
         }
         return $dataItem;
     }
