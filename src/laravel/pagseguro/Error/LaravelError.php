60d4dc300d14388fa2c1dc3171d455c5e852ba42
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2016-01-09 19:11:05 -0200

    Classe para tratativa dos erros

--- src/laravel/pagseguro/Error/LaravelError.php
+++ src/laravel/pagseguro/Error/LaravelError.php
@@ -22,7 +22,13 @@ class LaravelError
     public function __construct(\SimpleXMLElement $xmlObject) {
         $this->xmlObject = $xmlObject;
     }
-
+    
+    /**
+     * Método responsável por verificar a requisição se existe o chave de error
+     * @copyright (c) 2016, Michael Araujo
+     * @access public
+     * @since 0.1
+     */
     public function verifyErrors()
     {
         $array = $this->xml2array($this->xmlObject);
@@ -35,13 +41,26 @@ class LaravelError
         return $this;
     }
     
+    /**
+     * Método responsável por setar a string contendo os erros
+     * @copyright (c) 2016, Michael Araujo
+     * @access public
+     * @since 0.1
+     */
     public function ExceptionError()
     {
         if(count($this->errors)>0) {
             throw new \Exception("Error: ". implode("\n", $this->errors));
         }
     }
-
+    
+    /**
+     * Método responsável por transformar o objeto XML em Array
+     * @copyright (c) 2016, Michael Araujo
+     * @access private
+     * @since 0.1
+     * @param object contendo o xml da requisição
+     */
     private function xml2array($xmlObject)
     {
         return json_decode(json_encode($xmlObject), TRUE);
