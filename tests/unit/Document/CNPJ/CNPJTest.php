faa49b48f44b60ac5836d2c5a0b2bc189cb4954e
José Tobias de Freitas Neto <eneto.prof@gmail.com>
2018-01-12 22:45:07 -0400

    Testing CNPJ documents

---
+++ tests/unit/Document/CNPJ/CNPJTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace laravel\pagseguro\Tests\Unit\Sender\Document\CNPJ;
+
+use laravel\pagseguro\Document\CNPJ\CNPJ;
+
+/**
+ * Sender Document CNPJ Test
+ * @author José Tobias de Freitas Neto <jtfnetoo@gmail.com>
+ */
+class CNPJTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testEmptyCNPJ()
+    {
+        $cnpj = new CNPJ();
+        $this->assertEquals([
+            'number' => null,
+        ], $cnpj->toArray());
+    }
+
+    public function testCnpj()
+    {
+        $data = [
+            'number' => '51815418000198',
+        ];
+        $cnpj = new CNPJ($data);
+        $this->assertEquals($data, $cnpj->toArray());
+        $this->assertEquals($data['number'], $cnpj->getNumber());
+    }
+}
