ce8541c797309e4823b142f5c5d895f12953661f
Allan Wiese <allanwiese@gmail.com>
2023-02-04 16:51:36 -0300

    feat: suporte a notificações de pagametnos Pix

---
+++ src/laravel/pagseguro/Payment/Method/Pix/PixInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace laravel\pagseguro\Payment\Method\Pix;
+
+/**
+ * Payment Method Pix Interface
+ *
+ * @category   Payment
+ * @package    Laravel\PagSeguro\Payment
+ *
+ * @author     Allan Wiese <allanwiese@gmail.com>
+ * @since      2023-02-04
+ *
+ * @copyright  Laravel\PagSeguro
+ */
+interface PixInterface
+{
+    const PIX = 402;
+}
